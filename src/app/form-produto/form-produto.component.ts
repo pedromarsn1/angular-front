e90ee274c9139afe19114102597389cfd1a7026b
Pedro Nascimento <pedromarsn@gmail.com>
2022-04-22 13:28:54 -0300

    atualização 22/04/22

--- src/app/form-produto/form-produto.component.ts
+++ src/app/form-produto/form-produto.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {Produto} from "../produtos/produto/produto.model";
-import {ProdutoService} from "../shared/service/produto.service";
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import { Produto } from '../produtos/produto/produto.model';
+import { ProdutoService } from '../shared/service/produto.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UnidadeService } from '../shared/service/unidade.service';
 import { GrupoService } from '../shared/service/grupo.service';
 import { Unidade } from '../unidade/unidade.model';
@@ -11,48 +11,55 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-form-produto',
   templateUrl: './form-produto.component.html',
-  styleUrls: ['./form-produto.component.scss']
+  styleUrls: ['./form-produto.component.scss'],
 })
 export class FormProdutoComponent implements OnInit {
- public produtoForm!: FormGroup;
-  searchText : any
-  produtos: Produto []= []
-  unidades : Unidade[] = []
-  grupos : Grupo[] = []
+  public produtoForm!: FormGroup;
+  searchText: any;
+  produtos: Produto[] = [];
+  unidades: Unidade[] = [];
+  grupos: Grupo[] = [];
 
   constructor(
     private fb: FormBuilder,
-    public produtoService : ProdutoService,
-    public unidadeService : UnidadeService,
-    public grupoService : GrupoService,
-    private router : Router) { }
+    public produtoService: ProdutoService,
+    public unidadeService: UnidadeService,
+    public grupoService: GrupoService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.produtoForm = this.fb.group({
-      _id : ["", [Validators.required]],
-      codProduto: ["", [Validators.required]],
-      quantidade: ["", [Validators.required]],
-      grupo: ["", [Validators.required]],
-      nome: ["", [Validators.required]],
-      unidade: ["", [Validators.required]]
+      _id: ['', [Validators.required]],
+      codProduto: ['', [Validators.required]],
+      quantidade: ['', [Validators.required]],
+      grupo: ['', [Validators.required]],
+      nome: ['', [Validators.required]],
+      unidade: ['', [Validators.required]],
+    });
 
-    })
-
-   this.produtoService.getAll().subscribe(dados => this.produtos = dados)
-    this.unidadeService.getAll().subscribe(dados => this.unidades = dados)
-    this.grupoService.getAll().subscribe(dados =>this.grupos = dados)
-
+    this.produtoService.getAll().subscribe((dados) => (this.produtos = dados));
+    this.unidadeService.getAll().subscribe((dados) => (this.unidades = dados));
+    this.grupoService.getAll().subscribe((dados) => (this.grupos = dados));
   }
 
+  filterProductByCode(input: any) {
+    let produto = this.produtos.filter(
+      (produto) => produto.codProduto == input.target.value
+    )[0];
+    this.produtoForm.controls['nome'].setValue(produto.nome);
+    this.produtoForm.controls['unidade'].setValue(produto.unidade);
+    this.produtoForm.controls['grupo'].setValue(produto.grupo);
+    this.produtoForm.controls['quantidade'].setValue(produto.quantidade);
+    console.log(produto);
+  }
 
-  createProduto(){
-    this.produtoService.saveProduto(this.produtoForm.value).subscribe(()=>{
-      ((result: any) => result)
-      this.router.navigate(['/table-prod'])
-    })
+  createProduto() {
+    this.produtoService.saveProduto(this.produtoForm.value).subscribe(() => {
+      (result: any) => result;
+      this.router.navigate(['/table-prod']);
+    });
 
     this.produtoForm.reset();
   }
-
 }
-
